5e6e2cd92bef6c5078f56ce3fe72814f3a3c04d2
mollytaryn <mollytaryn@gmail.com>
2015-01-28 23:20:34 -0600

    displaying days of week

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,37 @@
 var weather_url = 'https://api.wunderground.com/api/181a3f71699e535c/forecast10day/q/TN/Nashville.json';
 
+//console.log testing
+
+/*function displayForecast(data){
+  var days = data.forecast.txt_forecast.forecastday;
+  console.log(days[0].title);
+  console.log(days[2].title);
+  console.log(days[4].title);
+  console.log(days[6].title);
+  console.log(days[8].title);
+}*/
+
+/*getJSON(weather_url, displayForecast);*/
+
+getJSON(weather_url, function(data){
+  var list = createList(data.forecast.simpleforecast.forecastday);
+  document.querySelector('ul').appendChild(list);
+});
+
+function createList(forecastDays){
+  var docFragment = document.createDocumentFragment();
+
+  forecastDays.forEach(function(forecastDay){
+    var li    = document.createElement('li'),
+        weekday = document.createTextNode(forecastDay.date.weekday);
+
+    li.appendChild(weekday);
+    docFragment.appendChild(li);
+  });
+
+  return docFragment;
+}
+
 function getJSON(url, cb) {
   var request = new XMLHttpRequest();
   request.open('GET', url);
@@ -12,13 +44,3 @@ function getJSON(url, cb) {
   request.send();
 }
 
-function displayForcast(data){
-  var days = data.forecast.txt_forecast.forecastday;
-  console.log(days[0]);
-  console.log(days[2]);
-  console.log(days[4]);
-  console.log(days[6]);
-  console.log(days[8]);
-}
-
-getJSON(weather_url, displayForcast);
